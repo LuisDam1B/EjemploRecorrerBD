95c11da1b05b7f73a9b4c58fe7368a73d27b4a68
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2019-12-18 14:08:19 +0100

    commit update 18/12/19

--- app/src/main/java/com/example/ejemplorecorrerbd/RecorrerBDFragment.java
+++ app/src/main/java/com/example/ejemplorecorrerbd/RecorrerBDFragment.java
@@ -1,8 +1,19 @@
 package com.example.ejemplorecorrerbd;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class RecorrerBDFragment extends Fragment {
 
-
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.recorrerbd_fragment,container,false);
+    }
 }
